5aff5e2b80ea0414aaff4fde012414dce779759f
hideyosh <buditriannggoro@gmail.com>
2025-11-29 15:07:39 +0700

    fixing problem ketika thumbnail di upload membuat 2 file

--- app/Filament/Resources/BookResource/Pages/CreateBook.php
+++ app/Filament/Resources/BookResource/Pages/CreateBook.php
@@ -13,32 +13,6 @@ class CreateBook extends CreateRecord
 {
     protected static string $resource = BookResource::class;
 
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        if (!empty($data['cover'])) {
-
-            $thumbnailPath = 'book-covers/thumbnails/' . basename($data['cover']);
-
-            $fullCoverPath = storage_path('app/public/' . $data['cover']);
-            $fullThumbPath = storage_path('app/public/' . $thumbnailPath);
-
-            // Pastikan folder ada
-            if (!is_dir(dirname($fullThumbPath))) {
-                mkdir(dirname($fullThumbPath), 0755, true);
-            }
-
-            $manager = new ImageManager(new Driver());
-            $manager->read($fullCoverPath)
-                ->resize(200, 300)
-                ->save($fullThumbPath);
-
-            $data['thumbnail'] = $thumbnailPath;
-        }
-
-        return $data;
-    }
-
-
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
