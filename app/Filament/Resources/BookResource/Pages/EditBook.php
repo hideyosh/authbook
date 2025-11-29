b0815e9a414873eefb51c46bd458c93efc9d1d9b
hideyosh <buditriannggoro@gmail.com>
2025-11-29 15:14:54 +0700

    fixing problem ketika thumbnail di upload membuat 2 file

--- app/Filament/Resources/BookResource/Pages/EditBook.php
+++ app/Filament/Resources/BookResource/Pages/EditBook.php
@@ -21,30 +21,6 @@ class EditBook extends EditRecord
         ];
     }
 
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        if (!empty($data['cover'])) {
-
-            $thumbnailPath = 'book-covers/thumbnails/' . basename($data['cover']);
-
-            $fullCoverPath = storage_path('app/public/' . $data['cover']);
-            $fullThumbPath = storage_path('app/public/' . $thumbnailPath);
-
-            if (!is_dir(dirname($fullThumbPath))) {
-                mkdir(dirname($fullThumbPath), 0755, true);
-            }
-
-            $manager = new ImageManager(new Driver());
-            $manager->read($fullCoverPath)
-                ->resize(200, 300)
-                ->save($fullThumbPath);
-
-            $data['thumbnail'] = $thumbnailPath;
-        }
-
-        return $data;
-    }
-
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
